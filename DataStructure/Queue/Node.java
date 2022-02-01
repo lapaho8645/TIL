1f42679262cfacb40d04f2c9e68aab04419c0e84
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-02-02 01:58:22 +0900

    Create Node.java

---
+++ DataStructure/Queue/Node.java
@@ -0,0 +1,32 @@
+package com.queue;
+
+public class Node {
+	private int data;
+	private Node next;
+
+	Node(int data) {
+		setData(data);
+		setNext(null);
+	}
+
+	Node() {
+		setNext(null);
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+}
